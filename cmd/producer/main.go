0c81f0a11546654c0066845188cbb844ed2e0c3a
Ayrton Souza <ayrtonsouza@icloud.com>
2021-10-25 00:54:55 -0300

    [:construction:] producer initial configuration

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
 	fmt.Println("Hello, go!")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "poc-kafka_kafka_1:9092",
+	}
+
+	producer, error := kafka.NewProducer(configMap)
+
+	if error != nil {
+		log.Println(error.Error())
+	}
+
+	return producer
 }
