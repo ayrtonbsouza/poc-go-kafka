171718d93fc70f4c5d411a2586ebcd82f2ae3928
Ayrton Souza <ayrtonsouza@icloud.com>
2021-11-02 01:49:55 -0300

    [:construction:] kafka consumer development

---
+++ cmd/consumer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "poc-kafka_kafka_1:9092",
+		"client.id":         "goapp-consumer",
+		"group.id":          "goapp-group",
+		"auto.offset.reset": "earliest",
+	}
+
+	consumer, error := kafka.NewConsumer(configMap)
+
+	if error != nil {
+		fmt.Println("error consumer", error)
+	}
+
+	topics := []string{"teste"}
+
+	consumer.SubscribeTopics(topics, nil)
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		}
+	}
+}
